d6b5198209065f31ddc3aa038773fa2be4fbaacc
kki2020 <kki2020@daum.net>
2023-01-13 18:07:24 +0900

    작업중

--- TUF.Host/Program.cs
+++ TUF.Host/Program.cs
@@ -1,5 +1,7 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Authentication.Cookies;
 
+var builder = WebApplication.CreateBuilder(args);
+string CorsName = "TufCors";
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
@@ -7,6 +9,24 @@ builder.Services.AddRazorPages();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(
+    CookieAuthenticationDefaults.AuthenticationScheme
+)
+.AddCookie();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(CorsName,
+    builder =>
+    {
+        builder
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .SetIsOriginAllowed(options => true)
+            .AllowCredentials();
+    });
+}); 
 
 var app = builder.Build();
 
@@ -17,11 +37,20 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
 
+app.UseCors(CorsName);
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseBlazorFrameworkFiles();
+app.UseRouting();
+app.MapRazorPages();
 app.MapControllers();
-
+app.MapFallbackToFile("index.html");
 app.Run();
